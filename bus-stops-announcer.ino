1b0a5f4ca1574e4a7f7fe469be2a9de0ff4c05d8
Sergey-Yushkov <151998367+Sergey-Yushkov@users.noreply.github.com>
2024-01-31 15:28:46 +0200

    Add display

--- bus-stops-announcer.ino
+++ bus-stops-announcer.ino
@@ -3,6 +3,22 @@
 #include <EncButton.h>
 #include <SoftwareSerial.h>
 #include <DFMiniMp3.h> // https://github.com/Makuna/DFMiniMp3
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 Button btn(BUTTON_PIN);
 
@@ -112,6 +128,26 @@ void setup() {
     trackCountInFolder1 = dfmp3.getFolderTrackCount(1);
     Serial.print("tracks in folder '01'");
     Serial.println(trackCountInFolder1);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+    }
+    // Show initial display buffer contents on the screen --
+    // the library initializes this with an Adafruit splash screen.
+    display.display();
+    delay(2000); // Pause for 2 seconds
+
+    // Clear the buffer
+    display.clearDisplay();
+    display.setTextSize(1);      // Normal 1:1 pixel scale
+    display.setCursor(0, 0);     // Start at top-left corner
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.cp437(true);         // Use full 256 char 'Code Page 437' font
+    display.write("Hello world!");
+    display.setCursor(20, 20);
+    display.write("TEST");
+    display.display();
 
     Serial.println("starting playing track 0001 in folder 01...");
     dfmp3.playFolderTrack16(1, 1); // start playing track '0001.....' in folder '01'
